7f8b41a0a5b7fcf59b6bd451a743bca40453adc3
Leandro Vieira <leandrorvieira22@yahoo.com>
2022-04-28 10:23:39 -0300

    first commit

---
+++ .eslintrc.js
@@ -0,0 +1,19 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: ['airbnb-base', 'prettier'],
+  plugins: ['prettier'],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  rules: {
+    'prettier/prettier': ['error', { endOfLine: 'auto' }],
+    'class-methods-use-this': 'off',
+    'no-params-reassign': 'off',
+    camelcase: 'off',
+    'no-unused-vars': ['error', { argsIgnorePattern: 'next' }],
+  },
+};
---
+++ src/config/auth.js
@@ -0,0 +1,4 @@
+export default {
+  secret: '61e110f46f6f9ccdc8c378e17a934485',
+  expiresIn: '7d',
+};
